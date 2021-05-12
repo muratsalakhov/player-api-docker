bbf4a8be5980ca50b0f832964460b9c668f7f25d
muratsalakhov <muratsalakhov@gmail.com>
2021-05-13 00:12:27 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
+use App\Services;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,61 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function () {
-    return Redis::set("boo", "foo");
+// ТЕСТ. Скачать zip
+Route::get('/test/', function () {
+    return Services\ProgramHandler::downloadProgramByUrl('http://127.0.0.1:84/zip/', storage_path() . '/app/public/5fc13fcaaa303a46ead63656/');
 });
 
+// ТЕСТ. Загрузить программу
+Route::get('/put-program/', function () {
+    $script = file_get_contents("/home/muratsalakhov/PhpstormProjects/player-service/player-api/storage/app/init_programs/api-chapter-mongo-id-new-2.json");
+    Redis::set("script:5fc13fcaaa303a46ead63656", $script);
+    return response(json_decode($script, true), 200)->header('Content-Type', 'application/json');
+});
+
+// Конвертировать изображения по переданному пути
+Route::put('/convert/', function (Request $request) {
+    $requestBody = json_decode($request->getContent(), true);
+    Services\ImageConverter::startConvert($requestBody['programPath']);
+    return response(array("status" => "ok"), 200)->header('Content-Type', 'application/json');
+});
+
+// Получить все сценарии из бд
+Route::get('/player/script/', function () {
+    $keys = Redis::keys("script:*");
+    $keys = array_map(function ($k){
+        return str_replace('laravel_database_', '', $k);
+    }, $keys);
+    $scripts = Redis::mget($keys);
+    foreach ($scripts as $scriptId => $script) {
+        $scripts[$scriptId] = json_decode($script);
+    }
+    return response($scripts, 200)->header('Content-Type', 'application/json');
+});
+
+// Получить сценарий по id
+Route::get('/player/script/{id}', function ($id) {
+    if (Redis::exists("script:" . $id)) {
+        return response(json_decode(Redis::get("script:" . $id), true), 200)->header('Content-Type', 'application/json');
+    } else {
+        return response('{"status" : "Script not found"}', 404)->header('Content-Type', 'application/json');
+    }
+});
+
+// Сохранения статистики прохождения
+Route::post('/player/statistic/{id}', function (Request $request, $id) {
+    return Redis::set("statistics:" . $id . ":" . time(), $request->getContent());
+});
+
+// Сохранения статистики прохождения
+Route::get('/player/statistic/{id}', function ($id) {
+    $keys = Redis::keys("statistics:" . $id . ":*");
+    $keys = array_map(function ($k){
+        return str_replace('laravel_database_', '', $k);
+    }, $keys);
+    $scripts = Redis::mget($keys);
+    foreach ($scripts as $scriptId => $script) {
+        $scripts[$scriptId] = json_decode($script);
+    }
+    return response($scripts, 200)->header('Content-Type', 'application/json');
+});
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/zip/', function () {
+    return response()->download('/home/muratsalakhov/Загрузки/Desktop (2)/program.zip');
+});
+
+// Выдача изображений
+Route::get('/data/{id}/{name}', function ($id, $name) {
+    $programPath = storage_path() . '/app/public/' . $id . '/';
+
+    try {
+        return response(file_get_contents($programPath . 'images-webp/' . $name . '.webp'), 200)->header('Content-Type', 'image/webp');
+    } catch (Throwable $e) {}
+
+    try {
+        return response(file_get_contents($programPath . 'images-png/' . $name), 200)->header('Content-Type', 'image/png');
+    } catch (Throwable $e) {}
+
+    return response(array("status" => "File not found"), 404)->header('Content-Type', 'application/json');
 });
